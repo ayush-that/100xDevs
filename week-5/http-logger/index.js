82fe3a0c7d1645dda0900fc1689fd1d5bc2efb95
Ayush Singh ☁️ <ayush1337@hotmail.com>
2024-10-30 06:40:57 +0530

    http-logger assignment finished

--- week-5/http-logger/index.js
+++ week-5/http-logger/index.js
@@ -3,4 +3,22 @@
 const express = require("express");
 const app = express();
 
+function consoleLogger(req, res, next) {
+  const httpMethod = req.method;
+  const url = req.url;
+  const timestamp = new Date();
+  console.log(
+    `HTTP Method: ${httpMethod}\nURL: ${url}\nTimestamp: ${timestamp}`
+  );
+  next();
+}
+
+app.use(consoleLogger);
+
+app.get("/", function (req, res) {
+  res.send(
+    "This server logs  logs each incoming request’s HTTP method, URL, and timestamp to the console."
+  );
+});
+
 app.listen(3000);
